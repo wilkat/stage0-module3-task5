ae607968b03e99911d08dbe19929d4a2a8d1ab49
wilkat <katarzyna.f.wilk@gmail.com>
2022-11-01 10:37:48 +0100

    new

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
-        double div = toBeDivided/5;
+        double div = toBeDivided/5.0;
         System.out.println(div);
     }
 }
